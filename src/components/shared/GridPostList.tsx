6ebd00395c485b8ae3106821fa78f5bd1d27e57b
adityaaaaaaaa1407 <adityasagavekar14@gmail.com>
2024-07-18 17:56:10 +0530

    explore

--- src/components/shared/GridPostList.tsx
+++ src/components/shared/GridPostList.tsx
@@ -23,7 +23,7 @@ const GridPostList = ({
         <li key={post.$id} className="relative min-w-80 h-80">
           <Link to={`/posts/${post.$id}`} className="grid-post_link">
             <img
-              src={post.imageUrl}
+              src={post?.imageUrl || "/assets/icons/image-placeholder.svg"}
               alt="post"
               className="h-full w-full object-cover"
             />
@@ -34,16 +34,16 @@ const GridPostList = ({
               <div className="flex items-center justify-start gap-2 flex-1">
                 <img
                   src={
-                    post.creator.imageUrl ||
+                    post?.creator?.imageUrl ||
                     "/assets/icons/profile-placeholder.svg"
                   }
                   alt="creator"
                   className="w-8 h-8 rounded-full"
                 />
-                <p className="line-clamp-1">{post.creator.name}</p>
+                <p className="line-clamp-1">{post?.creator?.name || "Unknown"}</p>
               </div>
             )}
-            {showStats && <PostStats post={post} userId={user.id} />}
+            {showStats && post && <PostStats post={post} userId={user.id} />}
           </div>
         </li>
       ))}
